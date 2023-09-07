696abf62b91032b3ee7b79f7fb30380f6dde72d2
Every1I <sharonmwangi056@gmail.com>
2023-09-07 06:16:24 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - serves as the starting point for program execution.
+ *
+ * Return: it is always 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
