f9333ec1b6aa3a9c6b1a30841910edb9d7fece11
Every1I <sharonmwangi056@gmail.com>
2023-09-11 16:53:11 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starting point for execution
+ *
+ * Return: ends execution of a function
+ */
+int main(void)
+{
+	int i;
+	char a;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (a = 'a' ; a <= 'f' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
