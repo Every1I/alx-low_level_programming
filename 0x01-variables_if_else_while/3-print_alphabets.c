36fe4a6fff44ba335e1263651d6e76c065d71a3e
Every1I <sharonmwangi056@gmail.com>
2023-09-11 16:03:50 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - starting point for execution
+ *
+ * Return: end execution of function
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+		putchar(a);
+	for (a = 'A' ; a <= 'Z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
