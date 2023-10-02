858d83ecdd4901d08316c47086cee699235bbc35
Every1I <sharonmwangi056@gmail.com>
2023-10-02 04:21:13 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Rreturn: 0 (success)
+ */
+int main(void)
+{
+	int dig1 = 0, dig2;
+
+	while (dig1 <= 99)
+	{
+		dig2 = dig1;
+		while (dig2 <= 99)
+		{
+			if (dig2 != dig1)
+			{
+				putchar((dig1 / 10) + 48);
+				putchar((dig1 % 10) + 48);
+				putchar(' ');
+				putchar((dig2 / 10) + 48);
+				putchar((dig1 % 10) + 48);
+
+				if (dig1 != 98 || dig2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			dig2++;
+		}
+		dig1++;
+	}
+	putchar('\n');
+	return (0);
+}
